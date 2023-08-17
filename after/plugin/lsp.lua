23967d360b9730a29c209635407207aa3fcd53a3
guilh <guilherme00rogerio@gmail.com>
2023-08-17 20:14:51 -0300

    Implement LSP for languages

---
+++ after/plugin/lsp.lua
@@ -0,0 +1,40 @@
+local lsp = require('lsp-zero')
+
+lsp.preset('recommended')
+lsp.ensure_installed ({
+  'tsserver',
+  'eslint',
+})
+
+require'lspconfig'.lua_ls.setup {
+  settings = {
+    Lua = {
+      diagnostics = {
+        -- Get the language server to recognize the `vim` global
+        globals = {'vim'},
+      },
+    },
+  },
+}
+
+local cmp = require('cmp')
+local cmp_select = {behavior = cmp.SelectBehavior.Select}
+local cmp_mappings = lsp.defaults.cmp_mappings({
+  ['<C-p>'] = cmp.mapping.select_prev_item(cmp_select),
+  ['<C-n>'] = cmp.mapping.select_next_item(cmp_select),
+  ['<Tab>'] = cmp.mapping.confirm({ select = true }),
+  ["<C-Space>"] = cmp.mapping.complete(),
+})
+
+cmp_mappings['<S-Tab>'] = nil
+
+lsp.setup_nvim_cmp({
+  mapping = cmp_mappings
+})
+
+lsp.set_preferences({
+    suggest_lsp_servers = false,
+    sign_icons = { Error = " ", Warning = " ", Hint = " ", Information = " " }
+})
+
+lsp.setup()
--- lua/plugins.lua
+++ lua/plugins.lua
@@ -7,31 +7,47 @@ end
 vim.cmd [[packadd packer.nvim]]
 
 packer.startup(function(use)
-  use 'wbthomason/packer.nvim'
-  use 'arcticicestudio/nord-vim'
-  use 'nvim-lualine/lualine.nvim'
-  use 'L3MON4D3/LuaSnip'
-  use 'ryanoasis/vim-devicons'
-  use 'preservim/nerdtree'
-  use 'tiagofumo/vim-nerdtree-syntax-highlight'
-  use 'Xuyuanp/nerdtree-git-plugin'
-  use 'nvim-lua/plenary.nvim'
-  use 'norcalli/nvim-colorizer.lua'
-  use 'nvim-telescope/telescope.nvim'
-  use 'nvim-telescope/telescope-file-browser.nvim'
+  use 'wbthomason/packer.nvim' -- Plugin manager
+  use 'arcticicestudio/nord-vim' -- Theme Nord
+  use 'nvim-lualine/lualine.nvim' -- Lualine buffer navigation
+  use 'ryanoasis/vim-devicons' -- Devicons
+  use 'preservim/nerdtree' -- Tree for navigation
+  use 'tiagofumo/vim-nerdtree-syntax-highlight' -- Nerdtree syntax highlight
+  use 'Xuyuanp/nerdtree-git-plugin' -- gitsigns plugin for nerdtree
+  use 'nvim-lua/plenary.nvim' -- plugin for telescope
+  use 'norcalli/nvim-colorizer.lua' -- Highlight in css color items
+  use 'nvim-telescope/telescope.nvim' -- File Finder
+  use 'nvim-telescope/telescope-file-browser.nvim' -- Plugin for File Finder
   use {
     'nvim-treesitter/nvim-treesitter',
     run = function() require('nvim-treesitter.install').update({ with_sync = true }) end,
-  }
-  use { 'neoclide/coc.nvim', branch = 'release' }
+  } -- Syntax Highlighth for languages
+  use {
+    'VonHeikemen/lsp-zero.nvim',
+    branch = 'v2.x',
+    requires = {
+      -- LSP Support
+      { 'neovim/nvim-lspconfig' },           -- Required
+      { 'williamboman/mason.nvim' },         -- Optional
+      { 'williamboman/mason-lspconfig.nvim' }, -- Optional
+      { 'glepnir/lspsaga.nvim' }, -- LSP Ui's
 
-  use 'windwp/nvim-autopairs'
-  use 'windwp/nvim-ts-autotag'
-  use 'onsails/lspkind-nvim'
-  use 'neovim/nvim-lspconfig'
-  use 'hrsh7th/cmp-buffer'
-  use 'hrsh7th/cmp-nvim-lsp'
-  use 'hrsh7th/nvim-cmp'
-  use 'williamboman/mason.nvim'
-  use 'williamboman/mason-lspconfig.nvim'
+      -- Autocompletion
+		  {'hrsh7th/nvim-cmp'},
+		  {'hrsh7th/cmp-buffer'},
+		  {'hrsh7th/cmp-path'},
+		  {'saadparwaiz1/cmp_luasnip'},
+		  {'hrsh7th/cmp-nvim-lsp'},
+		  {'hrsh7th/cmp-nvim-lua'},
+
+		  -- Snippets
+		  {'L3MON4D3/LuaSnip'},
+		  {'rafamadriz/friendly-snippets'},
+    }
+  } -- LSP
+
+  use 'windwp/nvim-autopairs' -- Auto pairs plugin
+  use 'windwp/nvim-ts-autotag' -- Auto tag for typescript
+
+  use 'lewis6991/gitsigns.nvim' -- Git signs plugin
 end)
