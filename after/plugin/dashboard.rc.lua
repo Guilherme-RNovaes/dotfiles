a68cbd078fa951fac0cb01031c688564d830ca97
guilh <guilherme00rogerio@gmail.com>
2023-09-02 14:55:40 -0300

    dashboard changes

--- after/plugin/dashboard.rc.lua
+++ after/plugin/dashboard.rc.lua
@@ -1,30 +1,42 @@
-local status, db = pcall(require, 'dashboard-nvim')
+local status, db = pcall(require, 'dashboard')
 if (not status) then return end
+
+vim.cmd[[
+  let g:indent_blankline_filetype_exclude = ['dashboard']
+]]
+
+local function key(shortcut)
+  shortcut.icon_hl = shortcut.icon_hl or "Title"
+  shortcut.desc_hl = shortcut.desc_hl or "String"
+  shortcut.key_hl = shortcut.key_hl or "Keyword"
+  return shortcut
+end
 
   db.setup{
     theme = 'doom', --  theme is doom and hyper default is hyper
     config = {
-      header = {}, --your header
+      header = {
+        "","", "", "", "", "","", "", "", "", "", "",
+        [[ ███▄    █     ▒█████      ██▓    ▄████▄     ▓█████   ]],
+        [[ ██ ▀█   █    ▒██▒  ██▒   ▓██▒   ▒██▀ ▀█     ▓█   ▀   ]],
+        [[▓██  ▀█ ██▒   ▒██░  ██▒   ▒██▒   ▒▓█    ▄    ▒███     ]],
+        [[▓██▒  ▐▌██▒   ▒██   ██░   ░██░   ▒▓▓▄ ▄██▒   ▒▓█  ▄   ]],
+        [[▒██░   ▓██░   ░ ████▓▒░   ░██░   ▒ ▓███▀ ░   ░▒████▒  ]],
+        [[░ ▒░   ▒ ▒    ░ ▒░▒░▒░    ░▓     ░ ░▒ ▒  ░   ░░ ▒░ ░  ]],
+        [[░ ░░   ░ ▒░     ░ ▒ ▒░     ▒ ░     ░  ▒       ░ ░  ░  ]],
+        [[   ░   ░ ░    ░ ░ ░ ▒      ▒ ░   ░              ░     ]],
+        [[         ░        ░ ░      ░     ░ ░            ░  ░  ]],
+        [[                                 ░                    ]],
+      "",""
+    }, --your header
       center = {
-        {
-          icon = ' ',
-          icon_hl = 'Title',
-          desc = 'Find File           ',
-          desc_hl = 'String',
-          key = 'b',
-          keymap = 'SPC f f',
-          key_hl = 'Number',
-          action = 'lua print(2)'
-        },
-        {
-          icon = ' ',
-          desc = 'Find Dotfiles',
-          key = 'f',
-          keymap = 'SPC f d',
-          action = 'lua print(3)'
-        },
+        key { icon = "  ", desc = "Telescope", key = "fw", action = "Telescope" },
+        key { icon = "  ", desc = "Find Files", key = "ff", action = "Telescope find_files" },
+        key { icon = "  ", desc = "Help", key = "fh", action = "Telescope help_tags" },
       },
-      footer = {}  --your footer
+      footer = {
+      ' My Dashboard',
+    }  --your footer
     },    --  config used for theme
     hide = {
       statusline = false,    -- hide statusline default is true
